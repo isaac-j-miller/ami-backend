6bf4c2c7538153ef161040dc39f2cf9befd319d3
isaac-j-miller <44651730+isaac-j-miller@users.noreply.github.com>
2020-01-22 18:46:45 -0500

    shell scripting

--- start-actual-hosting.sh
+++ start-actual-hosting.sh
@@ -1,9 +1,9 @@
 #ensure globals.js tells the front end to request from the actual host
 cd ~/git_repo/ami-backend/ami-front/src
-echo $"export default {\n\tvalue: 'http://3.219.163.17:8000'\n}" > 'globals.js'
+echo $"export default {value: 'http://3.219.163.17:8000'}" > 'globals.js'
 #turn debug mode on in django
 cd ~/git_repo/ami-backend/ami/ami
-sed -i 's/DEBUG_MODE\s?=\s?True/DEBUG_MODE = False/g' settings.py
-xterm -title 'backend server' -e 'python3 manage.py runserver 0.0.0.0:8000'
+sed -i 's/DEBUG\s?=\s?True/DEBUG = False/g' settings.py
+xterm -title 'backend server' -e 'python3 manage.py runserver 0.0.0.0:8000' &
 cd ~/git_repo/ami-backend/ami-front
-xterm -title 'frontend server' -e 'npm start'
+xterm -title 'frontend server' -e 'npm start' &
