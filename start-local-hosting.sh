d9064ce55d2ad656ddffa1905a27c17d16db5b53
isaac-j-miller <44651730+isaac-j-miller@users.noreply.github.com>
2020-01-22 18:36:07 -0500

    more typos

--- start-local-hosting.sh
+++ start-local-hosting.sh
@@ -1,6 +1,6 @@
 #ensure globals.js tells the front end to request from the localhost
 cd ~/git_repo/ami-backend/ami-front/src
-echo "export default {\n\tvalue:'http://127.0.0.1:8000'\n}" > 'globals.js'
+echo $"export default {\n\tvalue:'http://127.0.0.1:8000'\n}" > 'globals.js'
 #turn debug mode on in django
 cd ~/git_repo/ami-backend/ami
 sed -i 's/"DEBUG_MODE\s?=\s?False"/"DEBUG_MODE = True"/g' > 'settings.py'
