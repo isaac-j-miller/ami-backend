3d40ba97355c5f54eacda9655aad01e79d53cbd6
isaac-j-miller <44651730+isaac-j-miller@users.noreply.github.com>
2020-02-06 21:39:03 -0500

    update setup

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,11 +1,11 @@
 #FOR LINUX
 #install external stuff
+sudo apt install python3-pip
 sudo add-apt-repository ppa:ubuntugis/ppa
 sudo apt-get update
 sudo apt-get install unzip
 sudo apt-get install python3-gdal
 sudo apt install gdal-bin
-sudo apt install python3-pip
 sudo apt install awscli
 sudo apt install xterm
 sudo apt-get install git-core git-gui git-doc
@@ -18,18 +18,7 @@ npm install react-dropzone
 npm install -g serve
 npm i serve
 #install python libs
-pip3 install matplotlib
-pip3 install wheel
-pip3 install http://download.agisoft.com/Metashape-1.6.1-cp35.cp36.cp37-abi3-linux_x86_64.whl
-pip3 install django
-pip3 install pillow
-pip3 install numpy
-pip3 install django-cors-headers
-pip3 install djangorestframework
-pip3 install boto3
-pip3 install awscli
-pip3 install rasterio
-pip3 install gunicorn
+pip3 install -r requirements.txt
 #configure github
 git config --global user.name “isaac-j-miller”
 git config --global user.email “miller.isaac96@gmail.com”
